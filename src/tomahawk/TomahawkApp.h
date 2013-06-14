1d5d126beaae936cabf114d91c4e7f65d3e25e1b
Uwe L. Korn <uwelk@xhochy.com>
2013-06-14 20:19:05 +0200

    Add API 2.0 / Control API skeleton

--- src/tomahawk/TomahawkApp.h
+++ src/tomahawk/TomahawkApp.h
@@ -146,6 +146,8 @@ private:
 
     QPointer< QxtHttpServerConnector > m_httpv1_connector;
     QPointer< QxtHttpSessionManager > m_httpv1_session;
+    QPointer< QxtHttpServerConnector > m_httpv2_connector;
+    QPointer< QxtHttpSessionManager > m_httpv2_session;
 };
 
 Q_DECLARE_METATYPE( PairList )
